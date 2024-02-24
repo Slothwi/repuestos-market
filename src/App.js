5b06b8bfc6a023af8940ca19d1a5a3afda301f2f
Slothwi <gabo.leonp@gmail.com>
2024-02-24 00:23:36 -0300

    Modelo Base v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import Home from './views/Home';
+import Favs from './views/Favoritos';
+import Cart from './views/Cart';
+import Access from './views/Access';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from "./components/Navbar"
+import Publicar from './views/Publicar';
+import Ventas from './views/ventas';
+import Registro from './views/Registro';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='Access' element={<Access />} />
+        <Route path='Registro' element={<Registro />}/>
+        <Route path='/' element={<Home />} />
+        <Route path='Ventas' element={<Ventas />}/>
+        <Route path='Favs' element={<Favs />}/>
+        <Route path='Publicar' element={<Publicar />}/>
+        <Route path='Cart' element={<Cart />}/>
+      </Routes>
+      </ BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import { Navbar, Container } from "react-bootstrap";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../assets/css/Navbar.css"
+
+
+
+export default function Navigation() {
+    const setActiveClass = ({ isActive }) => (isActive ? "active" : "inactive");
+    const logo = "https://us.123rf.com/450wm/butenkov/butenkov2103/butenkov210300044/180573886-logotipo-vectorial-de-piezas-de-autom%C3%B3viles.jpg?ver=6"
+    
+    return (
+        <div>
+            <Navbar className="navbar">
+        <Container>
+            <Navbar.Brand>
+                <div className="img-container">
+                    <img className="carrito" src={logo} alt="repuestos" />
+                </div>
+            </Navbar.Brand>
+        <div className="nl">
+            <NavLink to="/Access" className={setActiveClass}>
+                Acceso
+            </NavLink>
+            <hr></hr>
+            <NavLink to="Registro" className={setActiveClass}>
+            Registro
+            </NavLink>
+            <hr></hr>
+            <NavLink to="/" className={setActiveClass}>
+                Home
+            </NavLink>
+            <hr></hr>
+            <NavLink to="/ventas" className={setActiveClass}>
+            Mis Publicaciones
+            </NavLink>
+            <hr></hr>
+            <NavLink to="/Favs" className={setActiveClass}>
+                Favs
+            </NavLink>
+            <hr></hr>
+            <NavLink to="/Publicar" className={setActiveClass}>
+                Publicar
+            </NavLink>
+            <hr></hr>
+            <NavLink to="Cart" className={setActiveClass}>
+            Carrito
+            </NavLink>
+        </div>
+        </Container>
+        </Navbar>
+        </div>
+    )
+
+}
